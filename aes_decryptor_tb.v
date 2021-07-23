b8b0dd96bcfbda19fd0b177c6f9ab3778db69a9c
akash-iitr <76245767+akash-iitr@users.noreply.github.com>
2021-07-23 14:34:25 +0530

    ciphertext updated

--- aes_decryptor_tb.v
+++ aes_decryptor_tb.v
@@ -9,7 +9,7 @@ module TB;
 	
 	initial
 	begin
-	   Test_State = 128'hff0b844a0853bf7c6934ab4364148fb9;
+	   Test_State = 128'hff0b844a0853bf7c6934ab4364148fa9;
 	   Test_Key = 128'h0f1571c947d9e8590cb7add6af7f6798;
 	end
 endmodule
